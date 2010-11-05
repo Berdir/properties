a25f1d2f325bebb920dbe7b2804ee906cd189d1f
Sascha Grossenbacher <saschagros@gmail.com>
2010-11-05 01:49:03 +0100

    Refactored category adding and storing in form state, drag and drop re-ordering works fine now.

--- properties-properties-form.tpl.php
+++ properties-properties-form.tpl.php
@@ -3,55 +3,62 @@
 $element = $variables['element'];
 $output = '';
 
-$table_id = 'attributes-values';
-$order_class = 'attributes-delta-order';
-$required = !empty($element['#required']) ? '<span class="form-required" title="' . t('This field is required. ') . '">*</span>' : '';
+$table_id = 'properties-table';
 
 $header = array(
-  array(
-    'data' => '<label>' . t('!title: !required', array('!title' => $element['#title'], '!required' => $required)) . "</label>",
-    'colspan' => 2,
-    'class' => array('field-label'),
-  ),
-  t('Label'),
+  t('Attribute name'),
+  t('Attribute label'),
+  t('Value'),
+  t('Category'),
   t('Order'),
 );
 $rows = array();
 
-// Sort items according to '_weight' (needed when the form comes back after
-// preview or failed validation)
-$items = array();
-foreach (element_children($element) as $key) {
-  if (is_int($key)) {
-    $items[] = &$element[$key];
-  }
-}
-usort($items, '_field_sort_items_value_helper');
+drupal_add_tabledrag($table_id, 'match', 'parent', 'property-category-select', 'property-category-select', 'category-name');
+drupal_add_tabledrag($table_id, 'order', 'sibling', 'property-weight');
 
-// Add the items as table rows.
-foreach ($items as $key => $item) {
-  $item['_weight']['#attributes']['class'] = array($order_class);
-  $delta_element = drupal_render($item['_weight']);
+foreach (element_children($element['listing']) as $category_name) {
+  $category = $element['listing'][$category_name];
+  $category['_weight']['#attributes']['class'] = array('property-weight');
+  $category['name']['#attributes']['class'] = array('category-name');
+  $category['category']['#attributes']['class'] = array('property-category-select');
 
-  $label = drupal_render($item['label']);
   $cells = array(
-    array('data' => '', 'class' => array('field-multiple-drag')),
-    array('data' => drupal_render($item), 'class' => array('properties-attribute-row-small')),
-    $label,
-    array('data' => $delta_element, 'class' => array('delta-order')),
+    'data' => array(
+      $category['#label'],
+      '',
+      '',
+      '',
+      drupal_render($category['category']) . drupal_render($category['name']) . drupal_render($category['_weight']),
+    ),
+    'class' => array('draggable', 'tabledrag-root'),
   );
-  $rows[] = array(
-    'data' => $cells,
-    'class' => array('draggable'),
-  );
+  $rows[] = $cells;
+
+  foreach (element_children($category['properties']) as $delta) {
+    $property = $category['properties'][$delta];
+
+    // Set special classes needed for table drag and drop.
+    $property['category']['#attributes']['class'] = array('property-category-select');
+    $property['_weight']['#attributes']['class'] = array('property-weight');
+
+    $cells = array(
+      array('data' => theme('indentation', array('size' => 1)) . drupal_render($property['attribute']), 'class' => array('properties-attribute-row-small')),
+      drupal_render($property['label']),
+      drupal_render($property['value']),
+      drupal_render($property['category']),
+      drupal_render($property['_weight']),
+    );
+    $rows[] = array(
+      'data' => $cells,
+      'class' => array('draggable', 'tabledrag-leaf'),
+    );
+  }
 }
 
 $output = '<div class="form-item">';
-$output .= theme('table', array('header' => $header, 'rows' => $rows, 'attributes' => array('id' => $table_id, 'class' => array('field-multiple-table'))));
-$output .= $element['#description'] ? '<div class="description">' . $element['#description'] . '</div>' : '';
+$output .= theme('table', array('header' => $header, 'rows' => $rows, 'attributes' => array('id' => $table_id, 'class' => array('properties-table'))));
 $output .= '<div class="clearfix">' . drupal_render($element['new_category']) . drupal_render($element['add_more']) . '</div>';
 $output .= '</div>';
-
-drupal_add_tabledrag($table_id, 'order', 'sibling', $order_class);
 
 echo $output;
